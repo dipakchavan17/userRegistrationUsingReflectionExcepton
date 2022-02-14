33388ef27b64fe545056f1688c5840b86b9d7478
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-14 17:30:50 +0530

    [dipak]Welcome to user registration

---
+++ UserRegistrationexRefAnn/UserRegistrationexRefAnn/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationexRefAnn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Using Exception,Reflection,mstest");
+            Console.ReadLine();
+        }
+    }
+}
