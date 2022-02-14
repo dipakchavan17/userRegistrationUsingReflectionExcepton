3edfb1f0e47f866e86e52a2f77da1edb85403c1e
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-14 18:04:37 +0530

    [dipak]validate Email Id

--- UserRegistrationexRefAnn/UserRegistrationexRefAnn/Program.cs
+++ UserRegistrationexRefAnn/UserRegistrationexRefAnn/Program.cs
@@ -8,14 +8,13 @@ namespace UserRegistrationexRefAnn
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void UserRegistration()
         {
-            Console.WriteLine("Welcome to User Registration Program");
             bool Continue = true;
             while (Continue)
             {
-                Console.WriteLine("Choose Option");
-                Console.WriteLine("1 = First Name\n2 = Last Name\n0=Exit");
+                Console.WriteLine("Choose Option to Validate with their specified Pattern");
+                Console.WriteLine("1 = First Name\n2 = Last Name\n3 =Email ID\n4 = Mobile Number\n5 = Password\n0 = Exit");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
@@ -32,13 +31,33 @@ namespace UserRegistrationexRefAnn
                         string lastName = Console.ReadLine();
                         Console.WriteLine(UserRegister.ValidateFirstName(lastName));
                         break;
+                    case 3:
+                        Console.WriteLine("Enter Email ID");
+                        string email = Console.ReadLine();
+                        Console.WriteLine(UserRegister.ValidateEmailID(email));
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter Mobile Number");
+                        string mobileNumber = Console.ReadLine();
+                        Console.WriteLine(UserRegister.ValidateMobileNumber(mobileNumber));
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter PassWord");
+                        string password = Console.ReadLine();
+                        Console.WriteLine(UserRegister.ValidatePassword(password));
+                        break;
                     default:
                         Console.WriteLine("Enter Correct Option!");
                         break;
                 }
             }
         }
-       
-       
+        //Program Entry Point
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            UserRegistration();
+            Console.ReadLine();
+        }
     }
 }
